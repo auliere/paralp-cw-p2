566ed67ea0e327d6948dbc03be590cd7bd21bcde
Oleg Pedorenko <olezhkoo@gmail.com>
2016-04-23 23:47:59 +0300

    Improved code and logs

--- src/Data.adb
+++ src/Data.adb
@@ -1,85 +1,97 @@
-with Ada.Text_IO; 			use Ada.Text_IO;
-with Ada.Integer_Text_IO; 	use Ada.Integer_Text_IO;
-with Ada.Float_Text_IO; 	use Ada.Float_Text_IO;
+---------------------------------------------------------
+------------------PARALLEL PROGRAMMING-------------------
+-------------------COURSE WORK PART 3--------------------
+----               --ADA. RENDEZVOUZ--              -----
+----              MA = MB*MC + MO*ME*a              -----
+---------------------------------------------------------
+-------------     BODY OF DATA MODULE     ---------------
+---------------------------------------------------------
+-----------------CREATED ON 20.03.2016-------------------
+----------------BY OLEG PEDORENKO, IP-31-----------------
+---------------------------------------------------------
+
+with Ada.Text_IO;       use Ada.Text_IO;
+with Ada.Integer_Text_IO;   use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
 with Ada.Containers.Generic_Constrained_Array_Sort;
 with Ada.Numerics.Float_Random;
 with Ada.Numerics.Discrete_Random;
 
 package body Data is
 
-	maxOutputSize: Integer := 8;
-			
-	procedure Get_Random(A: out Scalar) is
-		use Ada.Numerics.Float_Random;
-		subtype R is Integer range 1 .. Random_Max;
-		package Rand_Int is new Ada.Numerics.Discrete_Random (R);
-		G1: Generator;
-		G2: Rand_Int.Generator;
-	begin
-		Reset(G1); Rand_Int.Reset(G2);
-		A := Random(G1) * Scalar(Rand_Int.Random(G2));
-	end Get_Random;
-	
-	function Cmp(Left: Scalar; Right: Scalar) return Boolean;
-	procedure Desc_Sort is 
-		new Ada.Containers.
-			Generic_Constrained_Array_Sort(Range_T, Scalar, Vector, Cmp);
-	
-	procedure Vector_Input(A: out Vector);
-	procedure Vector_Output(A: in Vector);	
-	procedure Matrix_Input(A: out Matrix);
-	procedure Matrix_Output(A: in Matrix);
+  maxOutputSize: Integer := 8;
+      
+  procedure Get_Random(A: out Scalar) is
+    use Ada.Numerics.Float_Random;
+    subtype R is Integer range 1 .. Random_Max;
+    package Rand_Int is new Ada.Numerics.Discrete_Random (R);
+    G1: Generator;
+    G2: Rand_Int.Generator;
+  begin
+    Reset(G1); Rand_Int.Reset(G2);
+    A := Random(G1) * Scalar(Rand_Int.Random(G2));
+  end Get_Random;
+  
+  function Cmp(Left: Scalar; Right: Scalar) return Boolean;
+  procedure Desc_Sort is 
+    new Ada.Containers.
+      Generic_Constrained_Array_Sort(Range_T, Scalar, Vector, Cmp);
+  
+  procedure Vector_Input(A: out Vector);
+  procedure Vector_Output(A: in Vector);  
+  procedure Proto_Matrix_Input(A: out Proto_Matrix);
+  procedure Proto_Matrix_Output(A: in Proto_Matrix);
 
-	procedure Vector_Input(A: out Vector) is
-	begin
-		if Size <= maxOutputSize then
-			for I in Range_T loop
-				Input(A(I));
-			end loop;
-		else
-			for I in Range_T loop
-				Input(A(I));
-			end loop;		
-		end if;
-	end Vector_Input;
+  procedure Vector_Input(A: out Vector) is
+  begin
+    if Size <= maxOutputSize then
+      for I in A'Range loop
+        Input(A(I));
+      end loop;
+    else
+      for I in A'Range loop
+        Input(A(I));
+      end loop;    
+    end if;
+  end Vector_Input;
 
-	procedure Vector_Output(A: in Vector) is
-	begin
-		for I in Range_T loop
-			Output(A(I));
-		end loop;
-		New_Line;
-	end Vector_Output;
+  procedure Vector_Output(A: in Vector) is
+  begin
+    for I in A'Range loop
+      Output(A(I));
+    end loop;
+    New_Line;
+  end Vector_Output;
 
-	procedure Matrix_Input(A: out Matrix) is
-	begin
-		for I in Range_T loop
-			Vector_Input(A(I));
-		end loop;
-	end Matrix_Input;
-	
-	procedure Matrix_Output(A: in Matrix) is
-	begin
-		for I in Range_T loop
-			Vector_Output(A(I));
-		end loop;	
-	end Matrix_Output;	
-	
-	procedure Input(A: out Vector) is 
-	begin 
-		Vector_Input(A);
-		if Size <= maxOutputSize then 
-			Vector_Output(A);
-		end if;
-	end Input;
-	
-	procedure Input(A: out Matrix) is
-	begin 
-		Matrix_Input(A);
-		if Size <= maxOutputSize then
-			Matrix_Output(A);
-		end if;
-	end Input;
+  procedure Proto_Matrix_Input(A: out Proto_Matrix) is
+  begin
+    for I in A'Range loop
+      Vector_Input(A(I));
+    end loop;
+  end Proto_Matrix_Input;
+  
+  procedure Proto_Matrix_Output(A: in Proto_Matrix) is
+  begin
+    for I in A'Range loop
+      Vector_Output(A(I));
+    end loop;  
+  end Proto_Matrix_Output;  
+  
+  procedure Input(A: out Vector) is 
+  begin 
+    Vector_Input(A);
+    if Size <= maxOutputSize then 
+      Vector_Output(A);
+    end if;
+  end Input;
+  
+  procedure Input(A: out Proto_Matrix) is
+  begin 
+    Proto_Matrix_Input(A);
+    if Size <= maxOutputSize then
+      Proto_Matrix_Output(A);
+    end if;
+  end Input;
   
   procedure Input(A: out Scalar) is
   begin
@@ -87,25 +99,25 @@ package body Data is
     --Get_Random(A);
     A := 1.0;
   end Input;
-	
-	procedure Output(A: in Vector) is 
-	begin
-		if Size <= maxOutputSize then 
-			Vector_Output(A);
-		end if;
-	end Output;
-	
-	procedure Output(A: in Matrix) is 
-	begin
-		if Size <= maxOutputSize then
-			Matrix_Output(A);
-		end if;
-	end Output;
-	
-	procedure Output(A: in Scalar) is 
-	begin
-		Put(A, 5, 2, 0);
-	end Output;
+  
+  procedure Output(A: in Vector) is 
+  begin
+    if Size <= maxOutputSize then 
+      Vector_Output(A);
+    end if;
+  end Output;
+   
+  procedure Output(A: in Proto_Matrix) is 
+  begin
+    if Size <= maxOutputSize then
+      Proto_Matrix_Output(A);
+    end if;
+  end Output;
+  
+  procedure Output(A: in Scalar) is 
+  begin
+    Put(A, 7, 2, 0);
+  end Output;
   
   procedure Fill(A: out Vector) is
   begin
@@ -114,16 +126,16 @@ package body Data is
     end loop;
   end Fill;
   
-  procedure Fill(A: out Matrix) is
+  procedure Fill(A: out Proto_Matrix) is
   begin
     for I in A'Range loop
       Fill(A(I));
     end loop;
   end Fill;
 
-	function Cmp(Left: Scalar; Right: Scalar) return Boolean is
-	begin
-		return (Left > Right);
-	end Cmp;
-	
+  function Cmp(Left: Scalar; Right: Scalar) return Boolean is
+  begin
+    return (Left > Right);
+  end Cmp;
+  
 end Data;
--- src/p02.adb
+++ src/p02.adb
@@ -3,6 +3,9 @@
 -------------------COURSE WORK PART 2--------------------
 ----       --ADA. SEMAPHORES. PROTECTED UNITS--     -----
 ----              MA = MB*MC + MO*ME*a              -----
+---------------------------------------------------------
+-------------     MAIN MODULE PRG1     ------------------
+---------------------------------------------------------
 -----------------CREATED ON 20.03.2016-------------------
 ----------------BY OLEG PEDORENKO, IP-31-----------------
 ---------------------------------------------------------
@@ -10,8 +13,8 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Synchronous_Task_Control; use Ada.Synchronous_Task_Control;
-with Ada.Float_Text_IO; 	use Ada.Float_Text_IO;
-with Ada.Real_Time;			use Ada.Real_Time;
+with Ada.Float_Text_IO;   use Ada.Float_Text_IO;
+with Ada.Real_Time;      use Ada.Real_Time;
 with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Strings.Fixed; use Ada.Strings.Fixed;
 with Data;
@@ -21,31 +24,50 @@ procedure P02 is
   N: Integer := 8;
   P: Integer := 2;
   H: Integer := 4;
+-- t_start - start time; t_copy - copy start time; t_calc - calc start time; t_finish - finish time
+  t_start, t_copy, t_finish: Ada.Real_Time.Time;
+  t_calc: Ada.Real_Time.Time := Ada.Real_Time.Time_First;  
+  
+  procedure Write_Time_To_File(
+    Start_Time, End_Time: Ada.Real_Time.Time;
+    File_Name: String; Comment: String := "") is
+    Work_Time: Duration;
+    My_File: Ada.Text_IO.File_type;
+  begin
+    Work_Time := Ada.Real_Time.To_Duration(End_Time - Start_Time);
+    Put("N =   "); Put(N);
+    Put(Duration'Image(Work_Time)); Put("s. "); Put_Line("(" & Comment & ")");
+    
+    Create (File => My_File, Mode => Out_File, 
+      Name => File_Name);
+    Put(File => My_File, 
+      Item => Trim(Duration'Image(Work_Time), 
+                Ada.Strings.Left));
+    Put(File => My_File, Item => ";");
+    Close(My_File);  
+  end;
   
 begin
   --Read N and P from command line
-	if(Argument_Count > 0) then 
-		N := Integer'Value(Argument(1));
-	end if;
-  
+  if(Argument_Count > 0) then 
+    N := Integer'Value(Argument(1));
+  end if;  
   if(Argument_Count > 1) then 
-		P := Integer'Value(Argument(2));
-	end if;
+    P := Integer'Value(Argument(2));
+  end if;  
+  H := Integer(Float'Ceiling(Float(N)/Float(P)));
   
-  H := N/P;
-  Put(P);
-  Put(H);
   declare    
     -- Types
     package Data_S is new Data(N, 100); use Data_S;
     subtype Range_P is Integer range 1..(P);
     -- Variables
-		MA, MB, MC, MO, ME: aliased Matrix;
-		a: aliased Scalar;
-    
-    --SEMAPHORES
+    MA, MB, MC, MO, ME: aliased Matrix;
+    a: aliased Scalar;    
+    --SEMAPHORE
     Sem: array(Range_P) of Suspension_Object;
     
+    -- Protected module for shared resource
     protected Shared_Vars is
       procedure Write (MB, MO: in Matrix; a: in Scalar);
       entry Read (MB, MO: out Matrix; a: out Scalar);
@@ -53,11 +75,12 @@ begin
       MB_sh, MO_sh: Matrix;
       a_sh: Scalar;
       Read_Allowed: Boolean := False;
-    end Shared_Vars;
+    end Shared_Vars;    
     
     protected body Shared_Vars is
       procedure Write(MB, MO: in Matrix; a: in Scalar) is
       begin
+        Read_Allowed := False;
         MB_sh := MB;
         MO_sh := MO;
         a_sh := a;
@@ -72,6 +95,7 @@ begin
       end Read;
     end Shared_Vars;
     
+    -- Calculate MAh
     procedure Calculate
       (MBa, MCa, MOa, MEa: access Matrix; aa: access Scalar; Q: Integer) is
       Sum1, Sum2: Scalar;
@@ -87,17 +111,18 @@ begin
           Sum1 := 0.0;
           Sum2 := 0.0;
           for K in 1..N loop
-            Sum1 := Sum1 + (MBa.all(I)(K) * MCa.all(K)(J)); --MB * MCh
-            Sum2 := Sum2 + (MOa.all(I)(K) * MEa.all(K)(J)); -- MO * MEh
+            Sum1 := Sum1 + (MBa.all(I)(K) * MCa.all(J)(K)); --MB * MCh
+            Sum2 := Sum2 + (MOa.all(I)(K) * MEa.all(J)(K)); -- MO * MEh
           end loop;
-          MA(I)(J) := Sum1 + Sum2 * aa.all;
+          MA(J)(I) := Sum1 + Sum2 * aa.all;
         end loop;
       end loop;
     end;
     
-    -- OTHER TASKS --
+    
+    -- GENERIC TASK T1 or TX --
     task type Generic_Task(I: Integer) is
-      pragma Storage_Size(10000000);
+      pragma Storage_Size(100000000);
     end Generic_Task;
 
     task body Generic_Task is
@@ -115,18 +140,21 @@ begin
         Put_Line("MO =   "); Input(MO); 
         Put_Line("ME =   "); Input(ME);      
         Put_Line("a =    "); Input(a); Put(a); Put_Line("");
+        MB(2)(2) := 3.0;
+        t_copy := Clock;
         --Signal SN,1
         Shared_Vars.Write(MB, MO, a);      
-      end if;
-      
+      end if;      
       --Wait W1,1
       --Copy MB, MO, a
       Shared_Vars.Read(MB1, MO1, a1);
       --Calculate MAh
+      if(t_calc = Ada.Real_Time.Time_First) then
+        t_calc := Clock;
+      end if;
       Calculate(MB1'Access, MC'Access, MO1'Access, ME'Access, a1'Access, I - 1);      
       --Signal S1,1
-      Set_True(Sem(I));
-      
+      Set_True(Sem(I));      
       --For task T1
       if (I = 1) then
         --Wait WN,1
@@ -134,18 +162,34 @@ begin
           Suspend_Until_True(Sem(I));
         end loop;
         --Output MA
-        Put_Line("MA =   "); Output(MA);        
+        Put_Line("MA =   "); Output(MA);
+        
+        t_finish := Clock;
+        Write_Time_To_File(t_start, t_finish, 
+          Trim(Integer'Image(N), Ada.Strings.Left) & "-all",
+          "Total work time");
+        Write_Time_To_File(t_copy, t_finish, 
+          Trim(Integer'Image(N), Ada.Strings.Left) & "-copy_calc",
+          "Copy and calculation time");
+        Write_Time_To_File(t_calc, t_finish, 
+          Trim(Integer'Image(N), Ada.Strings.Left) & "-calc",
+          "Calculation time");
+        Put_Line("Task T1 finished");     
       end if;
       
       Put_Line("Task T" & Integer'Image(I) &" finished");      
     end Generic_Task;
     
-    T_Arr: array(Range_P) of access Generic_Task;
     
-  begin    
+    T_Arr: array(Range_P) of access Generic_Task;
+  
+  -- Generate P tasks
+  begin   
+    t_start := Clock;
+    
     for I in T_Arr'Range loop
       T_Arr(I) := new Generic_Task(I);
-    end loop;
+    end loop;    
   end;
   
 end P02;
